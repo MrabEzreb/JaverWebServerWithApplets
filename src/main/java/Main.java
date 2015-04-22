008bad8e46b629191f71d745e02220e2b019df28
MrabEzreb <de013b3f@opayq.com>
2015-04-22 11:42:08 -0400

    :(

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,7 @@ public class Main extends HttpServlet {
 	      throws ServletException, IOException {
 	  	resp.getWriter().println("<html><body>");
 	    resp.getWriter().println("<p>Hello from the Java Server!!!</p>");
-	    resp.getWriter().println("<applet code=\"appletTest.class\" width=\"300\" height =\"300\"></body></html>");
+	    resp.getWriter().println("<applet code=\"appletTestClass.class\" width=\"300\" height =\"300\"></body></html>");
   }
   
   private void sendApplet(HttpServletRequest req, HttpServletResponse resp)
